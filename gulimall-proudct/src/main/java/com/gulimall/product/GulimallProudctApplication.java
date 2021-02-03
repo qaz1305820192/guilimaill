66e602c6f42eee6f619fa61e61bf7310b3c86275
w jw <32531689+qaz1305820192@users.noreply.github.com>
2021-02-03 10:47:49 +0800

    test

---
+++ gulimall-proudct/src/main/java/com/gulimall/product/GulimallProudctApplication.java
@@ -0,0 +1,13 @@
+package com.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProudctApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProudctApplication.class, args);
+    }
+
+}
